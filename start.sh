79d97b54ead574c1a6c13494e367dafde2349fff
st4lk3r <christian@3bruggers.de>
2017-11-12 00:12:06 +0100

    added -t 0

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/local/bin/nheqminer -l ${POOL_URL} -u ${POOL_WORKER} -p x > /tmp/miner.log &
+/usr/local/bin/nheqminer -l ${POOL_URL} -u ${POOL_WORKER} -p x -t 0 > /tmp/miner.log &
 tail -f /tmp/miner.log
