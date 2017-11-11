76d0a16708276f03e81d6e3475aca28825015d74
st4lk3r <christian@3bruggers.de>
2017-11-12 00:00:14 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup /usr/local/bin/nheqminer -l eu.zec.slushpool.com:4444 -u st4lk3r0816.zworker1 -p x > /tmp/nohup.log &
-tail -f /tmp/nohup.log
+/usr/local/bin/nheqminer -l eu.zec.slushpool.com:4444 -u st4lk3r0816.zworker1 -p x > /tmp/miner.log &
+tail -f /tmp/miner.log
